f56afe5ab9c661ae5f33f27f715688ed983906d1
Justin Winslow <justin@jwin.me>
2014-10-26 15:32:00 -0500

    Initial style and ui build out

---
+++ public/js/main.js
@@ -0,0 +1,35 @@
+var url = 'https://www.googleapis.com/calendar/v3/calendars/';
+var calendar = 'rofff8a3lr670kseoioopm9ark@group.calendar.google.com';
+var key;
+var now = new Date().toISOString();
+var $eventsDefer;
+
+function getEvents(){
+  return $.ajax({
+    type: 'GET',
+    url: url + calendar + '/events?key=' + key + '&timeMin=' + now,
+    headers: {
+      'X-JavaScript-User-Agent':  'Google APIs Explorer'
+    }
+  });
+}
+
+$.get('key.json').done(function(response){
+  key = response.key;
+
+  $eventsDefer = getEvents();
+
+  $eventsDefer.done(function(response){
+    console.log(response);
+    $(document).ready(function(){
+      var $cal = $('.calendar ul');
+
+      $.each(response.items, function(index, item){
+        console.log(item);
+        var $li = $('<li>' + item.summary + '</li>');
+
+        $cal.append($li);
+      });
+    });
+  });
+});
